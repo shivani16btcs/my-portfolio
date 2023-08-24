435246f44887499369e54c910d55eae82b66e95e
shivani yadav <shivani16btcs0413@gmail.com>
2023-08-24 20:53:56 +0530

    setup for git page

--- script.js
+++ script.js
@@ -48,41 +48,6 @@ $(document).ready(function () {
       backSpeed: 50,
       loop: true
     });
-  
-    // var typed = new Typed(".typing-2", {
-    //   strings: [
-    //     "Fullstack Developer",
-    //     "Software Developer",
-    //     "Python Developer",
-    //     "Founder",
-    //     "Author"
-    //   ],
-    //   typeSpeed: 100,
-    //   backSpeed: 60,
-    //   loop: true
-    // });
-  
-    //  Owl Carousel  //
-  
-    // $(".carousel").owlCarousel({
-    //   margin: 20,
-    //   loop: true,
-    //   autoplay: true,
-    //   autoplayTimeOut: 2000,
-    //   autoplayHoverPause: true,
-    //   responsive: {
-    //     0: {
-    //       items: 1,
-    //       nav: false
-    //     },
-    //     600: {
-    //       items: 2,
-    //       nav: false
-    //     },
-    //     1000: {
-    //       items: 3,
-    //       nav: false
-    //     }
-    //   }
-    // });
   });
+
+
